0a670dce33e318e4600676adcda7eaea5947589d
Tajkia05 <44843998+Tajkia05@users.noreply.github.com>
2018-11-11 10:40:29 +0600

    Added drawer layout

--- app/src/main/java/com/example/team_rough/myapplication/MainActivity.java
+++ app/src/main/java/com/example/team_rough/myapplication/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.team_rough.myapplication;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+     private DrawerLayout dl;
+     private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dl = (DrawerLayout)findViewById(R.id.drlayout);
+        toggle = new ActionBarDrawerToggle(this,dl,R.string.open,R.string.close);
+        dl.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
