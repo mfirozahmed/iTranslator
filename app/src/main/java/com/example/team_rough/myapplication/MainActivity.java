5ec1c9815c3cbc61ebd9dec5231150d314ae4b0e
Rough22 <2210592+Rough22@users.noreply.github.com>
2018-11-14 12:17:03 +0600

    Fixed problems of merging

--- app/src/main/java/com/example/team_rough/myapplication/MainActivity.java
+++ app/src/main/java/com/example/team_rough/myapplication/MainActivity.java
@@ -1,17 +1,14 @@
 package com.example.team_rough.myapplication;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toast;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
