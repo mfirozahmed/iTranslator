36d7ec87d3fb37ec96f105dfbe2fc309d7ada735
snehadevi <snehadevi2016105@gmail.com>
2018-11-15 21:48:10 +0600

    Splashscreen added
    splashscreen java file added and run thread there, in the manifest lines added, fixed background and white effect transisting itranslator written image

--- app/src/main/java/com/example/team_rough/myapplication/MainActivity.java
+++ app/src/main/java/com/example/team_rough/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.team_rough.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
