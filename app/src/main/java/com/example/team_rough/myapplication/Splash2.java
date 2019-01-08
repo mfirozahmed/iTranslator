116361e7c1cbebf86ba166f0aa51dc1bc85c4228
snehadevi <snehadevi2016105@gmail.com>
2019-01-09 01:59:55 +0600

    Login added
    animation color background, some xml files, Login page, next = Forgot password to go to main activity, added

--- app/src/main/java/com/example/team_rough/myapplication/Splash2.java
+++ app/src/main/java/com/example/team_rough/myapplication/Splash2.java
@@ -1,18 +1,34 @@
 package com.example.team_rough.myapplication;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Handler;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class Splash2 extends AppCompatActivity {
 
-    private TextView textView;
-    private ImageView imageView;
+    RelativeLayout relativeLayout1, relativeLayout2, myLayout;
+    AnimationDrawable animationDrawable;
+    Button next;
+
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            relativeLayout1.setVisibility(View.VISIBLE);
+            relativeLayout2.setVisibility(View.VISIBLE);
+
+        }
+    };
 
 
     @Override
@@ -20,28 +36,25 @@ public class Splash2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash2);
 
-        textView = findViewById(R.id.tv);
-        imageView = findViewById(R.id.iv);
+        myLayout = (RelativeLayout) findViewById(R.id.myLayout);
 
-        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.alpha);
-        textView.startAnimation(myanim);
-        imageView.startAnimation(myanim);
+        animationDrawable = (AnimationDrawable) myLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(2000);
+        animationDrawable.start();
 
-        final Intent intent = new Intent(this,MainActivity.class);
-        Thread timer = new Thread(){
-            public void run(){
-               try{
-                   sleep(3000);
-               } catch (InterruptedException e){
-                   e.printStackTrace();
-               }
-               finally {
-                   Splash2.this.finish();
-                   startActivity(intent);
-               }
+        relativeLayout1 = (RelativeLayout) findViewById(R.id.relativeLayout1);
+        relativeLayout2 = (RelativeLayout) findViewById(R.id.relativeLayout2);
+
+        handler.postDelayed(runnable, 2000);
+        next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Splash2.this, MainActivity.class);
+                startActivity(intent);
             }
-        };
+        });
 
-        timer.start();
     }
 }
