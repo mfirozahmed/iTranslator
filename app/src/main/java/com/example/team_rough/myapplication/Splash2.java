cc21e77fc35b1bb92a57da36a96c49b5620accec
Tajkia05 <44843998+Tajkia05@users.noreply.github.com>
2019-01-10 01:58:38 +0600

    logout
    added logout button and connected to firebase

--- app/src/main/java/com/example/team_rough/myapplication/Splash2.java
+++ app/src/main/java/com/example/team_rough/myapplication/Splash2.java
@@ -4,21 +4,33 @@ import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Handler;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Splash2 extends AppCompatActivity {
+    EditText loginmail, loginpass;
+    Button signUpbtn;
 
     RelativeLayout relativeLayout1, relativeLayout2, myLayout;
     AnimationDrawable animationDrawable;
     Button next;
+    FirebaseAuth mAuth;
 
     Handler handler = new Handler();
     Runnable runnable = new Runnable() {
@@ -37,6 +49,8 @@ public class Splash2 extends AppCompatActivity {
 
         myLayout = findViewById(R.id.myLayout);
 
+        mAuth = FirebaseAuth.getInstance();
+
         animationDrawable = (AnimationDrawable) myLayout.getBackground();
         animationDrawable.setEnterFadeDuration(2000);
         animationDrawable.setExitFadeDuration(2000);
@@ -44,6 +58,11 @@ public class Splash2 extends AppCompatActivity {
 
         relativeLayout1 = findViewById(R.id.relativeLayout1);
         relativeLayout2 = findViewById(R.id.relativeLayout2);
+
+        //init
+        loginmail = (EditText)findViewById(R.id.mailAdress) ;
+        loginpass = (EditText)findViewById(R.id.passWord) ;
+        signUpbtn = (Button)findViewById(R.id.login);
 
         handler.postDelayed(runnable, 2000);
         next = (Button) findViewById(R.id.forget);
@@ -56,5 +75,50 @@ public class Splash2 extends AppCompatActivity {
             }
         });
 
+        signUpbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                allowingUserToLogin();
+            }
+        });
+
+    }
+
+    private void allowingUserToLogin() {
+
+        String email = loginmail.getText().toString();
+        String pass = loginpass.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(Splash2.this,"Please enter your mail", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        else if(TextUtils.isEmpty(pass)){
+            Toast.makeText(Splash2.this,"Please enter your password", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        else{
+            mAuth.signInWithEmailAndPassword(email, pass)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                sendUserToMainActivity();
+                            }
+                            else{
+                                Toast.makeText(Splash2.this, "Please check it again!!", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void sendUserToMainActivity() {
+        Intent mainIntent = new Intent(Splash2.this,MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
     }
 }
